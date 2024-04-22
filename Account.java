26c609f88dab0b28acb240ce3b8d7f9c40a1c57b
pomjoep <59618718+pomjoep@users.noreply.github.com>
2024-04-22 02:10:00 -0700

    2 almost done

--- Account.java
+++ Account.java
@@ -1,53 +1,54 @@
 /**
  * Account.java
- *
+ * <p>
  * This Account class represents bank accounts that stores money for an owner.
- *
- * Author: (put your name here)
+ * <p>
+ * Author: Quinn Epstein
  */
 class Account
 {
-// Instance variables
-	private double balance;	// invariant: balance >= 0 (should never be negative)
-	private String owner;
+    // Instance variables
+    private double balance;	// invariant: balance >= 0 (should never be negative)
+    private String owner;
 
-//Constructor
-	public Account(String name)
-	{
-		balance = 0;
-		owner = name;
-	}
+    //Constructor
+    public Account(String name)
+    {
+        balance = 0;
+        owner = name;
+    }
 
-// Instance methods
-	/** Deposits 'amount' into the account
-	 */
-	public void deposit(int amount)
-	{
-		balance = balance + amount;
-	}
+    // Instance methods
+    // Deposits 'amount' into the account
+    public void deposit(int amount)
+    {
+        balance += amount;
+    }
 
-	/** Withdraws 'amount' from the account.
-	 *	This only makes sense if amount <= balance.
-	 *	This method should be re-written so that there is not a possibility of
-	 *	having a negative balance.
-	 */
-	public void withdraw(int amount)
-	{
-		balance = balance - amount;
-	}
+    /** Withdraws 'amount' from the account.
+     *	This only makes sense if amount <= balance.
+     *	This method should be re-written so that there is not a possibility of
+     *	having a negative balance.
+     */
+    public void withdraw(int amount)
+    {
+        if(amount > balance){
+            System.out.printf("You cannot withdraw $%d%n", amount);
+            return;
+        }
+        balance = balance - amount;
+    }
 
-	/* Returns the balance of the Account
-	 */
-	public int getBalance()
-	{
-		return balance;
-	}
+    //Returns the balance of the Account
+    public double getBalance()
+    {
+        return balance;
+    }
 
-	/* Returns the owner of the Account
-	 */
-	public String getOwner()
-	{
-		return owner;
-	}
+    // Returns the owner of the Account
+    public String getOwner()
+    {
+        return owner;
+    }
 }
 
--- DebuggingExercise4.java
+++ DebuggingExercise4.java
@@ -1,8 +1,7 @@
 class DebuggingExercise4
 {
-    public static void main(String[] args)
-    {
-        Account a = null;
+    public static void main(String[] args) {
+        Account a = new Account("Darra");
         a.deposit(100);
         System.out.println(a.getOwner() + " has $" + a.getBalance());
         a.withdraw(200);
