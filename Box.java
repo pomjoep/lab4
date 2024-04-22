2ede3f406288f5eccc654642f8a81c9f26b7312c
pomjoep <59618718+pomjoep@users.noreply.github.com>
2024-04-22 01:16:39 -0700

    1 done

--- Box.java
+++ Box.java
@@ -2,61 +2,59 @@
 //The variable grade is a measure of the thickness of the cardboard
 //used to construct the box.
 
-public class Box2{
+public class Box {
 
-	private int width, depth;
-	private static int height, grade;
+    private int width, depth, height, grade;
 
-	//class constructor
-	public void Box2(int width, int height, int depth, int grade)
-	{
-		width = width;
-		height = height;
-		depth = depth;
-		grade = grade;
-	}
+    //class constructor
+    public Box(int width, int height, int depth, int grade){
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+        this.grade = grade;
+    }
 
-	//Two boxes should be considered equivalent if their volume is
-	//the same and they are constructed out of the same grade of cardboard.
-	//returns true if they are equivalent and false otherwise
-	public boolean equals(Box b)
-	{
-		return this.getVolume() == b.getVolume() && this.getGrade() = b.getGrade();
-	}
+    //Two boxes should be considered equivalent if their volume is
+    //the same and they are constructed out of the same grade of cardboard.
+    //returns true if they are equivalent and false otherwise
+    public boolean equals(Box b)
+    {
+        return this.getVolume() == b.getVolume() && this.getGrade() == b.getGrade();
+    }
 
-	//If this Box is larger than the argument Box b
-	//then return this Box - otherwise return b
-	public Box larger(Box b)
-	{
-		if(b.getVolume() > this.getVolume)
-			return this;
-		return b;
-	}
+    //If this Box is larger than the argument Box b
+    //then return this Box - otherwise return b
+    public Box larger(Box b)
+    {
+        if(this.getVolume() > b.getVolume())
+            return this;
+        return b;
+    }
 
-	//Simple getter methods
-	public int getGrade()
-	{
-		return g;
-	}
+    //Simple getter methods
+    public int getGrade()
+    {
+        return grade;
+    }
 
-	public int getVolume()
-	{
-		return width * height * depth * grade;
-	}
+    public int getVolume()
+    {
+        return width * height * depth;
+    }
 
-	public int getWidth()
-	{
-		return width;
-	}
+    public int getWidth()
+    {
+        return width;
+    }
 
-	public double getHeight()
-	{
-		return height;
-	}
+    public double getHeight()
+    {
+        return height;
+    }
 
-	public int getDepth(int b)
-	{
-		return b;
-	}
+    public int getDepth()
+    {
+        return depth;
+    }
 }
 
